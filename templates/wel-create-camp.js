b671408bb2bbc3fbb9fd44312110237ce87d37f7
Cortney <cortney@machinevantage.com>
2018-03-21 20:22:05 -0700

    created template literals for emails

--- templates/wel-create-camp.js
+++ templates/wel-create-camp.js
@@ -1,23 +1,23 @@
-const data = {
-    "id": 1,
-    "name": "Bulbasaur",
-    "base_experience": 64,
-    "height": 7,
-    "is_default": true,
-    "order": 1,
-    "weight": 69
-}
+// const data = {
+//     "id": 1,
+//     "name": "Bulbasaur",
+//     "base_experience": 64,
+//     "height": 7,
+//     "is_default": true,
+//     "order": 1,
+//     "weight": 69
+// }
 
-function createMarkup(data) {
-    return `
-        <article class="pokemon">
-            <h3>${data.name}</h3>
-            <p>The Pokemon ${data.name} has a base experience of ${data.base_experience}, they also weigh ${data.weight}</p>
-        </article>
-    `
-}
+// function createMarkup(data) {
+//     return `
+//         <article class="pokemon">
+//             <h3>${data.name}</h3>
+//             <p>The Pokemon ${data.name} has a base experience of ${data.base_experience}, they also weigh ${data.weight}</p>
+//         </article>
+//     `
+// }
 
-console.log(createMarkup(data))
+// console.log(createMarkup(data))
 
 
 // // const person = {
